fe4ee0fc19430c5b0710e1d9e36c0f4fffde1f60
Vladislav Atakhanov <eloh1mzee@gmail.com>
2025-08-22 20:32:54 +0500

    refactor: wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require 'wezterm';
-return {
+local act = wezterm.action
+
+local config = {
   default_prog = {"nu.exe"},
   color_scheme = "Apple System Colors",
   window_decorations = "RESIZE|TITLE",
@@ -7,33 +9,33 @@ return {
   leader = { key="a", mods="CTRL" },
   use_fancy_tab_bar = false,
   keys = {
-    { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-    { key = '"', mods = "LEADER|SHIFT",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "%",mods = "LEADER|SHIFT",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-    { key = "d", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+    { key = "a", mods = "LEADER|CTRL",  action=act{SendString="\x01"}},
+    { key = '"', mods = "LEADER|SHIFT", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
+    { key = "%", mods = "LEADER|SHIFT", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    { key = "c", mods = "LEADER",       action=act{SpawnTab="CurrentPaneDomain"}},
+    { key = "h", mods = "LEADER",       action=act{ActivatePaneDirection="Left"}},
+    { key = "j", mods = "LEADER",       action=act{ActivatePaneDirection="Down"}},
+    { key = "k", mods = "LEADER",       action=act{ActivatePaneDirection="Up"}},
+    { key = "l", mods = "LEADER",       action=act{ActivatePaneDirection="Right"}},
+    { key = "H", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Left", 5}}},
+    { key = "J", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Down", 5}}},
+    { key = "K", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Up", 5}}},
+    { key = "L", mods = "LEADER|SHIFT", action=act{AdjustPaneSize={"Right", 5}}},
+    { key = "&", mods = "LEADER|SHIFT", action=act{CloseCurrentTab={confirm=true}}},
+    { key = "d", mods = "LEADER",       action=act{CloseCurrentPane={confirm=true}}},
+    { key = "x", mods = "LEADER",       action=act{CloseCurrentPane={confirm=true}}},
+    { key = "H", mods = "SHIFT|ALT",    action=act.ActivateTabRelative(-1)},
+    { key = "L", mods = "SHIFT|ALT",    action=act.ActivateTabRelative(1)},
+    { key = "r", mods = "LEADER",       action=act.ReloadConfiguration },
   },
 }
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'LEADER',
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+return config
